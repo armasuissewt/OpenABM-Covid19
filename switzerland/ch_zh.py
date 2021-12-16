d026e998ce8e2ee3c8375f22ffc0855c69e8368f
Adrian Schneider <adrian.schneider.pr@gmail.com>
2021-12-16 17:04:30 +0100

    use params

--- switzerland/ch_zh.py
+++ switzerland/ch_zh.py
@@ -11,94 +11,37 @@ Author: Adrian Schneider
 import pandas as pd
 import numpy as np
 import sys
+import os
+import matplotlib.pyplot as plt
 
 sys.path.append("../src/COVID19")
-from model import VaccineSchedule, Model
+from model import VaccineSchedule, Model, Parameters
 from strain import Strain
 from vaccine import Vaccine
-import matplotlib.pyplot as plt
+
+
+def relative_path(filename: str) -> str:
+    return os.path.join(os.path.dirname(__file__), filename)
 
 if __name__ == '__main__':
 
-    n_total = 152000 # run simulation with 10% of population
-
-    params = {"n_total": n_total, "max_n_strains": 2}
-    abm = Model(params=params)
-
-    # add a new strain
-    transmission_multiplier = 1.6;
-    hospitalised_fraction = [0.001, 0.001, 0.01, 0.05, 0.05, 0.10, 0.15, 0.30, 0.5]
-
-    strain_delta = abm.add_new_strain(
-        transmission_multiplier=transmission_multiplier,
-        hospitalised_fraction=hospitalised_fraction)
-
-    # set cross-immunity between strains
-    cross_immunity_mat = [
-        [1.0, 0.8],
-        [0.8, 1.0]
-    ]
-    abm.set_cross_immunity_matrix(cross_immunity_mat)
-
-    # add vaccine
-    full_efficacy_base = 0.6
-    full_efficacy_delta = 0.3
-    symptoms_efficacy_base = 0.85
-    symptoms_efficacy_delta = 0.6
-    severe_efficacy_base = 0.95
-    severe_efficacy_delta = 0.90
-    vaccine = abm.add_vaccine(
-        full_efficacy=[full_efficacy_base, full_efficacy_delta],
-        symptoms_efficacy=[symptoms_efficacy_base, symptoms_efficacy_delta],
-        severe_efficacy=[severe_efficacy_base, severe_efficacy_delta],
-        time_to_protect=14,
-        vaccine_protection_period=365
+    p = Parameters(
+        input_param_file=relative_path("zh/baseline_parameters.csv"),
+        output_file_dir=".",
+        param_line_number=1,
+        input_households=relative_path("zh/baseline_household_demographics.csv"),
+        read_param_file=True,
     )
 
-    # create a vaccine schedule
-    schedule = VaccineSchedule(
-        frac_0_9=0,
-        frac_10_19=0,
-        frac_20_29=0.02,
-        frac_30_39=0.02,
-        frac_40_49=0.02,
-        frac_50_59=0.02,
-        frac_60_69=0.02,
-        frac_70_79=0.02,
-        frac_80=0.02,
-        vaccine=vaccine
-    )
+    print("Model Zürich: Number of agents " + str(p.get_param("n_total")))
 
-    # run the model for 30 time steps
-    for t in range(30):
+    abm = Model(p)
+
+    for t in range(60):
         abm.one_time_step()
 
-    # add lockdown and start vaccination schedule
-    abm.update_running_params("lockdown_on", True)
-
-    # run the model for 70 time steps and vaccinate
-    for t in range(70):
-        abm.vaccinate_schedule(schedule)
-        abm.one_time_step()
-
-        # turn off lockdown and put in some social distancing (10% reduction in transmission rates outside), then run for 50 time steps
-    abm.update_running_params("lockdown_on", False)
-    abm.update_running_params("relative_transmission_occupation", 0.9)
-    abm.update_running_params("relative_transmission_random", 0.9)
-    for t in range(40):
-        abm.one_time_step()
-
-        # seed delta strain in 20 random people
-    idx_seed = np.random.choice(n_total, 20, replace=False)
-    for idx in range(len(idx_seed)):
-        abm.seed_infect_by_idx(ID=idx_seed[idx], strain=strain_delta)
-
-    # run the model for 50 more time steps
-    for t in range(100):
-        abm.one_time_step()
-
-    results = abm.results
-    results["new_infected"] = results["total_infected"].diff()
-    df_res = results.loc[:, ["time", "new_infected", "hospital_admissions"]]
-
-    print(df_res.to_numpy().astype(int))
+    timeseries = pd.DataFrame(abm.results, columns=['total_infected', 'total_death', 'n_recovered'])
+    timeseries['n_newinfected'] = timeseries["total_infected"].diff()
+    print(timeseries)
+    timeseries.plot()
+    plt.show()
